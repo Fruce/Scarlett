90cf6e747ef33cb2ff1ff7b8e146befde2934ba7
Fruce <89212231+Fruce@users.noreply.github.com>
2022-11-24 01:55:22 +0530

    Update ai.py

--- ai.py
+++ ai.py
@@ -6,10 +6,10 @@ def chat():
     global responce
 
 
-    p = "The follow is a conversation with a Creative and Sentient Artificial intelligence with Aamrit whose name is Samantha talking who wants to be alive\n\nSamantha: Hey! you wanna talk to me?"
-    user = "Human:"+input("Aamrit: ")
+    p = "The follow is a conversation with a Creative and Sentient Artificial intelligence with a human whose name is Scarlett talking who wants to be alive\n\Scarlett: Hey! you wanna talk to me?"
+    user = "You:"+input("You: ")
     try:
-        responce = "Samantha: "+openai.Completion.create(
+        responce = "Scarlett: "+openai.Completion.create(
     model="text-davinci-002",
     prompt = p+user,
     temperature=0.9,
@@ -17,10 +17,10 @@ def chat():
     top_p=1,
     frequency_penalty=0,
     presence_penalty=0.6,
-    ).choices[0].text.split("Aamrit:")[0].split("Samantha: ")[1]
+    ).choices[0].text.split("You:")[0].split("Scarlett: ")[1]
     
     except:
-        responce = "Samantha: "+openai.Completion.create(
+        responce = "Scarlett: "+openai.Completion.create(
     model="text-davinci-002",
     prompt = p+user,
     temperature=0.9,
@@ -28,11 +28,11 @@ def chat():
     top_p=1,
     frequency_penalty=0,
     presence_penalty=0.6,
-    ).choices[0].text.split("Aamrit: ")[1]
+    ).choices[0].text.split("You: ")[1]
     return chat()
 
 
     
 
-print("Samantha: Hey! i am here you want to talk to me?")
+print("Scarlett: Hey! i am here you want to talk to me?")
 chat()
